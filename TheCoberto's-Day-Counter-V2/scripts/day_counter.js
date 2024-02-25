66ac6a2c7d00f4868b6b0d39251f199a62663d0d
Coby Kelly <coby.kelly.ck@gmail.com>
2024-02-25 01:34:16 -0500

    try passing data

--- TheCoberto's-Day-Counter-V2/scripts/day_counter.js
+++ TheCoberto's-Day-Counter-V2/scripts/day_counter.js
@@ -1,6 +1,28 @@
 import { world, system } from "@minecraft/server";
 
-world.afterEvents.worldInitialize.subscribe(() => {
-    let dayCountDisplay = system.createDisplay("sidebar", "Day Count");
-        dayCountDisplay.setDisplayObjective(world.getDay());
-});
+world.afterEvents.worldInitialize.subscribe(
+    (data) => {
+        event.player.onScreenDisplay.setTitle('Days:', {
+            subtitle: world.getDay()
+        });
+    }
+)
+
+
+// import { world, system } from "@minecraft/server";
+
+// world.afterEvents.worldInitialize.subscribe(
+//     (data) => {
+//         system.runInterval(
+//             () => {
+//                 if (world.getTimeOfDay() == 0)
+//                 {
+//                     let e = system.runInterval(() => {
+//                         world.sendMessage("Day: " + world.getDay());
+//                         system.clearRun(e)
+//                     })
+//                 };
+//             },
+//         )
+//     }
+// )
