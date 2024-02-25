186abaced9def778120911e78206e009ec758692
Coby Kelly <coby.kelly.ck@gmail.com>
2024-02-25 01:47:00 -0500

    4 tests

--- TheCoberto's-Day-Counter-V2/scripts/day_counter.js
+++ TheCoberto's-Day-Counter-V2/scripts/day_counter.js
@@ -2,6 +2,17 @@ import { world, system } from "@minecraft/server";
 
 world.afterEvents.worldInitialize.subscribe(
     (data) => {
+        system.runInterval(
+            () => {
+                if (world.getTimeOfDay() == 0)
+                {
+                    let e = system.runInterval(() => {
+                        world.sendMessage("Day: " + world.getDay());
+                        system.clearRun(e)
+                    })
+                };
+            }
+        )
         world.sendMessage("Ready! Day: " + world.getDay());
             let dayCountDisplay = system.createDisplay("sidebar", "DayCount");
                 dayCountDisplay.setDisplayObjective(world.getDay());
